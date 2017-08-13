5bc0f6fc5a131900f39c037b8f094a1a48f8a6e0
moritalous <moritalous@gmail.com>
2017-08-13 10:36:57 +0900

    Modify shell

--- start.sh
+++ start.sh
@@ -1,10 +1,12 @@
 # /bin/sh
 
+# create voice
+tmux new-session -d  'cd ~/MyRobot2017Summer/web && ./create_voice.sh'
+
 # start web
-tmux new-session -d  'cd ~/MyRobot2017Summer/web && npm run liveserver'
+tmux new-session -d  'cd ~/MyRobot2017Summer/web && npm install && npm run build && npm run liveserver'
 
 sleep 60
 
 # start touchphat
 tmux new-session -d 'python3 ~/MyRobot2017Summer/touchphat/buttons.py'
-
