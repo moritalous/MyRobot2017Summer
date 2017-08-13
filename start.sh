b111a34161c841f4c34252c12fe7a55cf42a4445
moritalous <moritalous@gmail.com>
2017-08-13 10:57:03 +0900

    Mod script

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@
 tmux new-session -d  'cd ~/MyRobot2017Summer/web && ./create_voice.sh'
 
 # start web
-tmux new-session -d  'cd ~/MyRobot2017Summer/web && npm install && npm run build && npm run liveserver'
+tmux new-session -d  'cd ~/MyRobot2017Summer/web && npm run build && npm run liveserver'
 
 sleep 60
 
